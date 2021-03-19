eb66fec739cb40e7576bcfaa72edbc5cf0c295d2
patrickm68 <patrick.mcenaney68@gmail.com>
2021-03-20 00:53:33 +0200

    update favicons (#171)

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,9 +2,10 @@ module.exports = {
   title: 'CodeceptJS', // Title for your website.
   description: 'SuperCharged End 2 End Testing with WebDriver & Puppeteer',
   head: [
-    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' }],
     ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' }],
     ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' }],
+    ['link', { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon/favicon.ico' }],
     ['link', { rel: 'manifest', href: '/favicon/site.webmanifest' }],
     ['link', { rel: 'mask-icon', href: '/favicon/safari-pinned-tab.svg', color: '#805ad5' }],
     ['meta', { name: 'theme-color', content: '#805ad5' }],

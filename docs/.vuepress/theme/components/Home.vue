275404cc3165731dfff168cba8df9875964aabd3
patrickm68 <patrick.mcenaney68@gmail.com>
2021-09-16 00:36:04 +0300

    updated title

--- docs/.vuepress/theme/components/Home.vue
+++ docs/.vuepress/theme/components/Home.vue
@@ -163,10 +163,10 @@
   <h2 class="text-center">Trusted By Enterprises</h2>
   <div class="companies">
 
-    <img src="/img/companies/doxyme.png" >
-    <img src="/img/companies/gen3.png" style="background: #666; padding: 10px;" class="rounded" >
-    <img src="/img/companies/isotipo.png" class="rounded" >
-    <img src="/img/companies/mynd.png" class="rounded" >
+    <img src="/img/companies/doxyme.png" title="Doxy.me" alt="Doxy.me" >
+    <img src="/img/companies/gen3.png" title="Gen3"  alt="Gen3" style="background: rgb(143 156 150); padding: 10px;" class="rounded" >
+    <img src="/img/companies/isotipo.png" title="Isitopio"  alt="Isitopio" class="rounded" >
+    <img src="/img/companies/mynd.png" title="Mynd"  alt="Mynd" class="rounded" >
 
   </div>
 
@@ -284,8 +284,9 @@ export default {
   border-top: 5px dashed;
   @apply bg-white mt-4 py-8 border-gray-200;
   .companies {
+    margin-left: -50px;
     max-width: 1200px;
-    @apply flex items-center;
+    @apply flex items-center mb-4;
     img {
       filter: grayscale(0.9);
       @apply flex-none rounded-xl;
